4f752c7c07b7b5ec3ed9f6df47652318f1cd4444
Ian Holmes <concord511@gmail.com>
2021-09-18 21:37:03 -0500

    created the functionality, updated HTML, and tested most functions

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+// assign the currentDay id tag the current date in requested format
+let currentDate = moment().format("dddd, MMM Do");
+$("#currentDay").text(currentDate);
+
+// assign an empty object to hold tasks
+let currentTasks = {};
+
+// function that checks the current time against hour blocks and assigns them their corresponding color
+const auditTime = function() {
+    $(".description").each(function() {
+        let currentHour = 14; // parseInt(moment().hour());
+        let hourBlock = parseInt($(this).attr("id").replace("div", ""));
+        if (currentHour > hourBlock) {
+            $(this).addClass("past");
+        }
+        else if (currentHour === hourBlock) {
+            $(this).addClass("present");
+        }
+        else if (currentHour < hourBlock) {
+            $(this).addClass("future");
+        }
+    });
+}
+
+// function that savesTasks
+const saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(currentTasks));
+}
+
+// function that loads the current tasks and assign their text values to their corresponding IDs.
+const loadTasks = function() {
+    currentTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (!currentTasks) {
+        currentTasks = {
+            9: "",
+            10: "",
+            11: "",
+            12: "",
+            13: "",
+            14: "",
+            15: "",
+            16: "",
+            17: ""
+        };
+    }
+    else {
+        $.each(currentTasks, function(id, text) {
+            $("#div" + id).text(text);
+        });
+    }
+}
+
+// on-click event for editing hourly tasks
+$(".time-block").on("click", "div", function() {
+    let id = $(this).attr("id").replace("row", "");
+    let divEl = $("#" + id);
+    let divClasses = divEl.attr("class");
+    let divText = divEl.text();
+    let textInput = $("<textarea>").addClass(divClasses).val(divText);
+    textInput.attr("id", id);
+    divEl.replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+// on-click event for saving hourly tasks
+$(".time-block").on("click", ".saveBtn", function() {
+    let id = $(this).attr("id").replace("btn", "");
+    let textEl = $("#div" + id);
+    let divText = textEl.val().trim();
+    let divEl = $("<div>")
+        .attr("id", "div" + id)
+        .addClass(textEl.attr("class"))
+        .text(divText);
+    textEl.replaceWith(divEl);
+    currentTasks[id] = divText;
+    console.log(currentTasks);
+    saveTasks();
+});
+
+// assign hour blocks colors based on past, present, future of current time
+auditTime();
+
+// set an interval to call auditTime() every minute
+setInterval(auditTime(), 60000);
+
+// call the function to load tasks from localStorage
+loadTasks();
